f42a47a9e49534fcb5ac9ca11b26f1f88865f2aa
Macy Graham-Kimbrough <macyg196502@yahoo.com>
2018-02-08 09:10:12 -0600

    Receiving valid response

--- request-gif.js
+++ request-gif.js
@@ -14,22 +14,24 @@ $(document).ready(function() {
  */
 function fetchAndDisplayGif(event) {
     
-    // This prevents the form submission from doing what it normally does: send a request (which would cause our page to refresh).
-    // Because we will be making our own AJAX request, we dont need to send a normal request and we definitely don't want the page to refresh.
+    // This prevents the form submission from doing what it normally does: send a request (which would cause our
+    // page to refresh).
+    // Because we will be making our own AJAX request, we dont need to send a normal request and we definitely
+    // don't want the page to refresh.
     event.preventDefault();
     
     // get the user's input text from the DOM
-    var searchQuery = ""; // TODO should be e.g. "dance"
+    var searchQuery = $("input[name='tag']").val(); // TODO should be e.g. "dance"
 
     // configure a few parameters to attach to our request
     var params = { 
         api_key: "dc6zaTOxFJmzC", 
-        tag : "" // TODO should be e.g. "jackson 5 dance"
+        tag : "jackson 5 " + searchQuery // TODO should be e.g. "jackson 5 dance"
     };
     
     // make an ajax request for a random GIF
     $.ajax({
-        url: "", // TODO where should this request be sent?
+        url: "https://api.giphy.com/v1/gifs/random", // TODO where should this request be sent?
         data: params, // attach those extra parameters onto the request
         success: function(response) {
             // if the response comes back successfully, the code in here will execute.
@@ -37,10 +39,22 @@ function fetchAndDisplayGif(event) {
             // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
             console.log("we received a response!");
             console.log(response);
-            
+                        
+            console.log("Response data " + response.data);
             // TODO
             // 1. set the source attribute of our image to the image_url of the GIF
             // 2. hide the feedback message and display the image
+            console.log("Parsing the response");
+            $("#feedback").text("");
+            setGifLoadedStatus(false);
+
+//          console.log("Response data " +  response.data("image_url"));
+            for (var element in response){
+                console.log("The data is" + element);
+            }
+
+            
+
         },
         error: function() {
             // if something went wrong, the code in here will execute instead of the success function
@@ -53,7 +67,8 @@ function fetchAndDisplayGif(event) {
     
     // TODO
     // give the user a "Loading..." message while they wait
-    
+    $("#feedback").text("Your image is loading...");
+    setGifLoadedStatus(false);
 }
 
 
